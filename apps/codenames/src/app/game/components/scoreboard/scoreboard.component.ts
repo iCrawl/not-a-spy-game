eb3ecd0e073b7edf75f1376b8d526ba5f760b956
iCrawl <icrawltogo@gmail.com>
2020-01-05 21:48:56 +0100

    feat: active turn display

--- apps/codenames/src/app/game/components/scoreboard/scoreboard.component.ts
+++ apps/codenames/src/app/game/components/scoreboard/scoreboard.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Scoreboard } from '@codenames/models';
+import { Game } from '@codenames/models';
 import { select, Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { App } from '../../../models/app.model';
@@ -7,16 +7,17 @@ import { App } from '../../../models/app.model';
 @Component({
 	selector: 'codenames-scoreboard',
 	template: `
-		<span class="red">{{ (score$ | async)?.red }}</span> - <span class="blue">{{ (score$ | async)?.blue }}</span>
+		<span class="red" [class.turn]="(game$ | async)?.turn === 0">{{ (game$ | async)?.scoreboard?.red }}</span> -
+		<span class="blue" [class.turn]="(game$ | async)?.turn === 1">{{ (game$ | async)?.scoreboard?.blue }}</span>
 	`,
 	styleUrls: ['./scoreboard.component.scss'],
 })
 export class ScoreboardComponent implements OnInit {
-	public score$!: Observable<Scoreboard>;
+	public game$!: Observable<Game>;
 
 	public constructor(private readonly store: Store<App>) {}
 
 	public ngOnInit() {
-		this.score$ = this.store.pipe(select(state => state.game.scoreboard));
+		this.game$ = this.store.pipe(select(state => state.game));
 	}
 }
